89c7b365be2e0de7123dc1840e7abb9a5db29ff6
Allen Wen <acw6923@rit.edu>
2024-09-27 14:59:27 -0500

    GridMover: move multiple ref centers

--- GridMover/src/gridmover.cc
+++ GridMover/src/gridmover.cc
@@ -9,7 +9,14 @@ extern "C" void GridMover_Move_Grids(CCTK_ARGUMENTS)
   DECLARE_CCTK_PARAMETERS;
   DECLARE_CCTK_ARGUMENTS;
 
-	position_x[index_region] = path_radius * cos(path_freq * cctk_time);
-	position_y[index_region] = path_radius * sin(path_freq * cctk_time);
-	position_z[index_region] = 0;
+	position_x[index_region1] = -path_radius * cos(path_freq * cctk_time);
+	position_y[index_region1] = -path_radius * sin(path_freq * cctk_time);
+	position_z[index_region1] = 0;
+
+	if (index_region2 != -1) {
+		position_x[index_region2] = path_radius * cos(path_freq * cctk_time);
+		position_y[index_region2] = path_radius * sin(path_freq * cctk_time);
+		position_z[index_region2] = 0;
+	}
+	
 }
